45f558ebf4c22628d60250e0bdf49ac0314ae5ef
deubong <33288932+deubong@users.noreply.github.com>
2018-06-10 16:47:02 +0900

    LCD에 시간과 온습도를 측정하여 출력하는 파이썬 코드
    LCD에 시간과 온습도를 측정하여 출력하는 파이썬 코드

---
+++ LCD_date_temp_humi.py
@@ -0,0 +1,33 @@
+from Adafruit_CharLCD import Adafruit_CharLCD
+import Adafruit_DHT
+from time import sleep
+from datetime import datetime
+import time
+
+turn = 4
+
+sensor = Adafruit_DHT.DHT22
+pin=18
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+lcd = Adafruit_CharLCD(rs=22, en=11, d4=23, d5=10, d6=9, d7=25, cols=16, lines=2)
+
+count = 0
+
+while True:
+    message = datetime.today().strftime("   %p %I:%M:%S\n")
+    lcd.clear()
+    
+    if count >= 0 and count < turn:
+        message = message + datetime.today().strftime("Date  %Y.%m.%d\n")
+    elif count >= turn and count < (turn*2):
+        message = message + ('Temp       {0:0.1f}C'.format(temperature))
+    elif count >= (turn*2) and count < (turn*3):
+        message = message + ('Humidity   {0:0.1f}%'.format(humidity))
+    lcd.message(message)
+    
+    count = count + 1
+    if count == (turn*3) :
+        count = 0
+        
+    sleep(1)
